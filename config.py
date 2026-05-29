56cc80a3f357057b008e0f81f79bfe730bb533ba
José Valença <jmvalenca@gmail.com>
2026-05-29 10:54:14 +0100

    inicializar uma versão pura MPC

--- config.py
+++ config.py
@@ -9,7 +9,7 @@
 
 import marimo
 
-__generated_with = "0.23.5"
+__generated_with = "0.23.8"
 app = marimo.App(width="medium")
 
 with app.setup:
@@ -66,12 +66,13 @@ class config_NP:
 @app.class_definition
 @dataclass
 class config_MPC:
-    rsize : int = 8   # "rate" em bytes
-    csize : int = 24  # "capacity" em bytes
+    rsize : int = 8   # "rate" em wires de wsize bytes
+    csize : int = 16  # "capacity" em wires de wsize bytes
     rounds: int = 8   # numero de "rounds" por permutação
-    ksize : int = 16  
-    wsize : int = 2
-    iosize: int = 32
+    ksize : int = 16  # bytes por cada seed
+    wsize : int = 2   # bytes por wire
+    iosize: int = 24
+    sessions: int = 32  #
 
 
 if __name__ == "__main__":
